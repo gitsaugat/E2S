340c3324bec328cd405128127b41c78425ddbf26
gitsaugat <letnotsay@gmail.com>
2022-08-18 06:46:48 +0545

    remove import

--- maps/settings.py
+++ maps/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from importlib.resources import path
 from pathlib import Path
 import os
 
